6d7eaa4e29aadfebced7132e1a7bd1c7a2ddf6ac
KarunNagaraj <karun.nagraj@gmail.com>
2026-02-15 18:59:45 +0530

    added auth route details

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,6 +12,7 @@ dotenv.config();
 const app = express();
 const PORT= process.env.PORT;
 
+app.use(express.json())
 app.use('/api/user', userRoutes)
 app.use('/api/auth', authRoutes)
 app.use('/api/admin', adminRoutes)
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,8 +1,26 @@
 import { Router } from "express";
+import { User } from "../models/user.model";    
 
 const router = Router();
 
-router.get('/',(req,res)=>{
-    res.send("auth route");
+router.post('/callback',async (req,res)=>{
+    try{
+    const {id, firstName, lastName, imageUrl} = req.body;
+    const user = User.findOne({clerkId:id})
+    if(!user){
+        //signup
+        await User.create({
+            fullName:`${firstName} ${lastName}`,
+            ImageUrl:imageUrl,
+            clerkId:id
+        })
+    }
+    res.status(200).json({message:"success"})
+    }
+    catch(error){
+        console.log("error in auth callback",error);
+        res.status(500).json({message:"Internal server error"})
+    }
+
 });
 export default router;
