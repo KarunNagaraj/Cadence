d160bb180d088800b3b949be7bb7fd6ec791ce55
KarunNagaraj <karun.nagraj@gmail.com>
2026-02-15 17:53:36 +0530

    Models and mongodb conenction stuff

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,6 +6,7 @@ import authRoutes from './routes/auth.route.js';
 import songRoutes from './routes/song.route.js';
 import albumRoutes from './routes/album.route.js';
 import statsRoutes from './routes/stats.route.js';
+import { connectDB } from './lib/db.js';
 
 dotenv.config();
 const app = express();
@@ -21,5 +22,6 @@ app.use('/api/stats', statsRoutes)
 
 app.listen(PORT,()=>{
     console.log("Server is running on port "+PORT);
+    connectDB();
 })
 
