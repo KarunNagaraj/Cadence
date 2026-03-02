71ca7ba7b1c62b17b5bf93dad6ccbfb4632daa5d
KarunNagaraj <karun.nagraj@gmail.com>
2026-03-02 17:31:57 +0530

    socket io

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,18 +1,3 @@
-/*import express from 'express';
-import dotenv from 'dotenv';
-import userRoutes from './routes/user.route.js';
-import connectDB from './lib/db.js';
-import cors from 'cors';
-dotenv.config();
-const app = express();
-const PORT= process.env.PORT;
-
-app.use(cors())
-app.use('/api/user', userRoutes)
-app.listen(PORT,()=>{
-    console.log("Server is running on port "+PORT);
-    connectDB();
-})*/
 import express from "express";
 import dotenv from "dotenv";
 import { clerkMiddleware } from "@clerk/express";
@@ -20,17 +5,18 @@ import fileUpload from "express-fileupload";
 import path from "path";
 import cors from "cors";
 import fs from "fs";
-
+import { createServer } from "http";
 import cron from "node-cron";
 
+import { initializeSocket } from "./lib/socket.js";
 
-import connectDB  from "./lib/db.js";
+import { connectDB } from "./lib/db.js";
 import userRoutes from "./routes/user.route.js";
 import adminRoutes from "./routes/admin.route.js";
 import authRoutes from "./routes/auth.route.js";
 import songRoutes from "./routes/song.route.js";
 import albumRoutes from "./routes/album.route.js";
-import statRoutes from "./routes/stats.route.js";
+import statRoutes from "./routes/stat.route.js";
 
 dotenv.config();
 
@@ -38,7 +24,8 @@ const __dirname = path.resolve();
 const app = express();
 const PORT = process.env.PORT;
 
-
+const httpServer = createServer(app);
+initializeSocket(httpServer);
 
 app.use(
 	cors({
@@ -95,7 +82,7 @@ app.use((err, req, res, next) => {
 	res.status(500).json({ message: process.env.NODE_ENV === "production" ? "Internal server error" : err.message });
 });
 
-app.listen(PORT, () => {
+httpServer.listen(PORT, () => {
 	console.log("Server is running on port " + PORT);
 	connectDB();
 });
